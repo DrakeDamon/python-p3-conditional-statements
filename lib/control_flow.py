6c1995a8f06269f2115b6ee53d54a047401bc944
Drake Damon <dddamon06@gmail.com>
2024-12-16 21:51:06 -0500

    practicing for phase 3 exam

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,50 +1,62 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    if (username == "admin" or username == "ADMIN") and password == "12345":
+    if username.upper() == 'ADMIN' and password == '12345':
         return "Access granted"
-    else:
+    else: 
         return "Access denied"
 
 
+
+
+# /*
+#   Write a method `hows_the_weather` that takes in one argument, a temperature.
+#   If the temperature is below 40, return "It's brisk out there!". If the temperature is
+#   between 40 and 65, return "It's a little chilly out there!". If the temperature is above 85,
+#   return "It's too dang hot out there!". Otherwise, return "It's perfect out there!"
+# */
 def hows_the_weather(temperature):
     if temperature < 40:
         return "It's brisk out there!"
-    elif 40 <= temperature <= 65:
+    elif temperature >= 40 and temperature <= 65:
         return "It's a little chilly out there!"
     elif temperature > 85:
         return "It's too dang hot out there!"
     else:
         return "It's perfect out there!"
 
-    
 
-
+# /* 
+#   Write a method `fizzbuzz` takes in a number. For multiples of three, return
+#   "Fizz" instead of the number. For the multiples of five, return "Buzz". For
+#   numbers which are multiples of both three and five, return "FizzBuzz". For
+#   all other numbers, just return the number itself.
+# */
 def fizzbuzz(num):
-    if num%3==0 and num%5==0:
-        return 'FizzBuzz'
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    if num % 3 == 0:
+        return "Fizz"
+    if num % 5 == 0:
+        return "Buzz"
+    return num
 
-    elif num % 3 == 0:
-        return 'Fizz'
 
-    elif num % 5==0:
-        return 'Buzz'
-    else:
-        return num
-
+# /*
+#   Write a method `calculator` that takes three arguments: an operation and two
+#   numbers. If the operation is one of the following: `+`, `-`, `*`, or `\`,
+#   return the value of calling the operation on the two numbers. Otherwise,
+#   output a message saying "Invalid operation!" and return `nil`.
+# */
 def calculator(operation, num1, num2):
-    if operation == '+':
+    if operation == "+":
         return num1 + num2
-    elif operation == '-':
+    elif operation == "-":
         return num1 - num2
-    elif operation == '*':
+    elif operation == "*":
         return num1 * num2
-    elif operation == '/':
-        if num2 == 0:
-            return "Division by zero not allowed."
-        else:
-            return num1 / num2
+    elif operation == "/":
+        return num1 / num2
     else:
         print("Invalid operation!")
-        return None
-
+        return None      
